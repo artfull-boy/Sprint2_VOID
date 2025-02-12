f5c7c40bd3af37a9eaeca2b2d97c0c97dbe94bd8
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-12 14:55:51 +0100

    Update newsletter.js

--- newsletter.js
+++ newsletter.js
@@ -1,4 +1,5 @@
 const TestimonialSection = document.querySelector('#TestimonialSection');
+window.location.href.includes("index.html") ? 
 TestimonialSection.insertAdjacentHTML('afterend', `<section class="bg-[#2F1893]">
     <section
       class="min-xl:px-[315px] pb-[100px] min-md:px-[200px] py-[70px] min-sm:px-[100px] px-[30px] flex flex-col gap-[60px]">
@@ -18,6 +19,28 @@ TestimonialSection.insertAdjacentHTML('afterend', `<section class="bg-[#2F1893]"
         </div>
         <button type="submit" id="subscribe"
           class="bg-[#fff] cursor-pointer h-[45px] text-black rounded hover:bg-yellow-50 transition-colors duration-300">Subscribe</button>
+      </form>
+    </section>
+  </section>`)
+  : TestimonialSection.insertAdjacentHTML('afterend', `<section class="bg-[#2F1893]">
+    <section
+      class="min-xl:px-[315px] pb-[100px] min-md:px-[200px] py-[70px] min-sm:px-[100px] px-[30px] flex flex-col gap-[60px]">
+      <div class="flex flex-col gap-[26px]">
+        <p id="newsletter-title" class="text-[42px] max-sm:text-3xl leading-[52px] font-bold text-white">
+                اشترك في نشرتنا الإخبارية
+        </p>
+        <p id="newsletter-description" class="text-white">
+                ابق على اطلاع بآخر الأخبار والعروض الخاصة.
+        </p>
+      </div>
+      <form class="flex flex-col gap-[26px]" onsubmit="handleNewsletterSubmit(event)">
+        <div class="flex flex-col gap-[10px]">
+          <label id="form-field5"  for="newsletter-email" class="text-white">لبريد الإلكتروني</label>
+          <input type="email" id="newsletter-email" name="newsletter-email"
+            class="border-[1px] border-[#fff] rounded-[5px] p-[10px] text-white" required />
+        </div>
+        <button type="submit" id="subscribe"
+          class="bg-[#fff] cursor-pointer h-[45px] text-black rounded hover:bg-yellow-50 transition-colors duration-300">اشترك</button>
       </form>
     </section>
   </section>`);
