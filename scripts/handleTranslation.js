c5d1e86025aab23dec9df9b92c691980c8b4167a
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-13 17:45:44 +0100

    Update handleTranslation.js

--- scripts/handleTranslation.js
+++ scripts/handleTranslation.js
@@ -2,7 +2,7 @@ const arabic = document.getElementById('ar');
 const english = document.getElementById('en');
 
 const handleTranslate = () => {
-        if (window.location.href.includes("index.html")) {
+        if ((window.location.href.includes("index.html")||window.location.pathname=="/") && !window.location.href.includes("arabic.html")) {
                 english.classList.add('font-bold');
                 arabic.classList.remove('font-bold');
         } else {
@@ -12,5 +12,5 @@ const handleTranslate = () => {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-        handleTranslate();
+handleTranslate();
 });
