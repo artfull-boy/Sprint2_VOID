5a64e4cb91fd8f5a3522f6a219ebf58921603e15
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-13 12:50:25 +0100

    Create handleShowMore.js

---
+++ scripts/handleShowMore.js
@@ -0,0 +1,13 @@
+const additional_Content = document.getElementById('additional_Content');
+const showMore = document.getElementById('showMore');
+
+const handleShowMore = () => {
+  if (additional_Content.classList.contains('hidden')) {
+    additional_Content.classList.remove('hidden');
+    window.location.href.includes("index.html") ? showMore.innerHTML = 'Show Less' : showMore.innerHTML = 'أظهر أقل';
+  }
+  else {
+    additional_Content.classList.add('hidden');
+    window.location.href.includes("index.html") ? showMore.innerHTML = 'Show More' : showMore.innerHTML = 'أظهر المزيد';
+  }
+}
