dc281fd9a0279e5bd55e725ee45dc83f4d803777
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-14 17:21:31 +0100

    Update handleTranslation.js

--- scripts/handleTranslation.js
+++ scripts/handleTranslation.js
@@ -1,3 +1,4 @@
+const htmlElement = document.documentElement;
 const arabic = document.getElementById('ar');
 const english = document.getElementById('en');
 
@@ -5,9 +6,11 @@ const handleTranslate = () => {
         if ((window.location.href.includes("index.html")||window.location.pathname=="/") && !window.location.href.includes("arabic.html")) {
                 english.classList.add('font-bold');
                 arabic.classList.remove('font-bold');
+                htmlElement.removeAttribute("dir")
         } else {
                 arabic.classList.add('font-bold');
                 english.classList.remove('font-bold');
+                htmlElement.setAttribute("dir","rtl")
         }
 }
 
