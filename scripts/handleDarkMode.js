5d692555423f4f28909a7ff998f52b8e877a4373
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-14 17:20:39 +0100

    Update handleDarkMode.js

--- scripts/handleDarkMode.js
+++ scripts/handleDarkMode.js
@@ -1,13 +1,27 @@
-const htmlElemt = document.documentElement;
 const darkModeHandler = document.getElementById('darkModeHandler');
+const htmlElemt = document.documentElement;
 
 const handleDarkMode = (event) => {
-        event.srcElement.classList.toggle('fa-toggle-off');
-        event.srcElement.classList.toggle('fa-toggle-on');
         if (htmlElemt.getAttribute("data-theme") == 'dark') {
                 htmlElemt.setAttribute("data-theme", "light");
+                localStorage.setItem('theme', 'light');
+                event.srcElement.classList.add('fa-toggle-off');
+                event.srcElement.classList.remove('fa-toggle-on');
+
         } else {
                 htmlElemt.setAttribute("data-theme", "dark");
+                localStorage.setItem('theme', 'dark');
+                event.srcElement.classList.remove('fa-toggle-off');
+                event.srcElement.classList.add('fa-toggle-on');
         }
 }
 darkModeHandler.addEventListener('click', handleDarkMode);
+document.addEventListener('DOMContentLoaded', () => {
+        if (localStorage.getItem('theme') == 'dark') {
+                htmlElemt.setAttribute("data-theme", "dark");
+                darkModeHandler.classList.add('fa-toggle-on');
+        } else {
+                htmlElemt.setAttribute("data-theme", "light");
+                darkModeHandler.classList.add('fa-toggle-off');
+        }
+});
