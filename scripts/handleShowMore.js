ad81024319e5ada82f6f996f2c40463beac4d1ef
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-14 21:53:40 +0100

    Update handleShowMore.js

--- scripts/handleShowMore.js
+++ scripts/handleShowMore.js
@@ -4,11 +4,11 @@ const showMore = document.getElementById('showMore');
 const handleShowMore = () => {
   if (additional_Content.classList.contains('hidden')) {
     additional_Content.classList.remove('hidden');
-    window.location.href.includes("index.html") ? showMore.innerHTML = 'Show Less' : showMore.innerHTML = 'أظهر أقل';
+    (window.location.href.includes("index.html") || window.location.pathname=="/") ? showMore.innerHTML = 'Show Less' : showMore.innerHTML = 'أظهر أقل';
   }
   else {
     additional_Content.classList.add('hidden');
-    window.location.href.includes("index.html") ? showMore.innerHTML = 'Show More' : showMore.innerHTML = 'أظهر المزيد';
+    (window.location.href.includes("index.html") || window.location.pathname=="/") ? showMore.innerHTML = 'Show More' : showMore.innerHTML = 'أظهر المزيد';
   }
 }
 showMore.addEventListener('click', handleShowMore);
